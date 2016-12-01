65ac24f0109674c08818fc45c2b84e417c7c9c33
Rishabh Singh <rishabh-singh@uiowa.edu>
2016-11-30 20:33:05 -0600

    changes

--- WordMagic/ViewController.swift
+++ WordMagic/ViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var HelpLabel: UILabel!
-
+    
+    var cardView: UIView!
+    var back: UIImageView!
+    var front: UIImageView!
+    var showingBack = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let text:String = "This is a word learning app. \n Words are contained in Sets, sets are random group of words. Words are fetched from server which we have build from scratch and stored in Core Data inside app. \n\nUSAGE \nCurrently, Set feature is complete.To go back from a screen to previous screen just Swipe Right. In the word meaning Screen where you seen words, meaning and sentences, you can swipe up and down to see the next and previous word respectively. \n\n NOTE: For the first time, app will take some time as it would be loading data from server. Screen may be blank for several seconds."
-        HelpLabel.text = text
+        front = UIImageView(image: UIImage(named: "image1.jpg"))
+        back = UIImageView(image: UIImage(named: "back.jpg"))
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector (tapped))
+      //  var tap = UITapGestureRecognizer(target: self, action: Selector("tappedMe"))
+        back.addGestureRecognizer(singleTap)
+        back.isUserInteractionEnabled = true
+        singleTap.numberOfTapsRequired = 1
+        
+        let rect = CGRect(x:20,y: 20,width: (back.image?.size.width)!,height: (back.image?.size.height)!)
+        cardView = UIView(frame: rect)
+        cardView.addSubview(back)
+        cardView.addGestureRecognizer(singleTap)
+        cardView.isUserInteractionEnabled = true
+        view.addSubview(cardView)
+        
       
     }
 
@@ -24,6 +42,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tapped() {
+        if (showingBack) {
+            UIView.transition(from: back, to: front, duration: 1, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+            showingBack = false
+        } else {
+            UIView.transition(from: front, to: back, duration: 1, options: UIViewAnimationOptions.transitionFlipFromLeft, completion: nil)
+            showingBack = true
+        }
+    }
 
 }
 
