765d788a57d5e833f14366acba089dfc69f4a758
Haider Hameed <haiderhameed@gmail.com>
2016-12-03 12:44:13 -0600

    sr algo basic interface

---
+++ SpacedRepetitionAlgo.swift
@@ -0,0 +1,41 @@
+//
+//  SpacedRepetitionAlgo.swift
+//  WordMagic
+//
+//  Created by Haider Hameed on 12/3/16.
+//  Copyright © 2016 Rishabh Singh. All rights reserved.
+//
+
+import Foundation
+
+class SpacedRepetitionAlgo {
+    func has_next() -> Bool {
+        return false
+    }
+    
+    func get_next() -> Word {
+        return Word()
+    }
+    
+    func mark_yes() {
+    }
+    
+    func mark_no() {
+    }
+    
+    func get_total() -> Int {
+        return 0
+    }
+    
+    func get_mastered() -> Int {
+        return 0
+    }
+    
+    func get_reviewing() -> Int {
+        return 0
+    }
+    
+    func get_learning() -> Int {
+        return 0
+    }
+}
